98b40c410640b4cffd275102864a6e06da85a649
Jens Christiansen <>
2018-04-14 11:31:08 -0600

    view can change

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import {List} from "./Components/Views/List";
+import {Note} from "./Components/Views/Note";
 
 export default class App extends Component {
     constructor(props) {
@@ -8,10 +9,20 @@ export default class App extends Component {
             notes : [],
             view : List,
         };
+        this.showNoteView = this.showNoteView.bind(this);
+        this.showListView = this.showListView.bind(this);
+    }
+    showNoteView(){
+       this.setState({view: Note});
+    }
+    showListView(){
+       this.setState({view: List});
     }
     render() {
         return <this.state.view
             notes={this.state.notes}
+            showNoteView={this.showNoteView}
+            showListView={this.showListView}
         />;
     }
 }
