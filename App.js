5af1acdb663d8c78bc0d4a56658d29d52e3cbdbe
Jens Christiansen <>
2018-04-14 11:53:44 -0600

    app can create a note

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {ListView} from "./components/views/List";
 import {NoteView} from "./components/views/Note";
+import {Note} from "./models/Note";
 
 export default class App extends Component {
     constructor(props) {
@@ -11,6 +12,7 @@ export default class App extends Component {
         };
         this.showNoteView = this.showNoteView.bind(this);
         this.showListView = this.showListView.bind(this);
+        this.createNote = this.createNote.bind(this);
     }
 
     showNoteView() {
@@ -21,11 +23,17 @@ export default class App extends Component {
         this.setState({view: ListView});
     }
 
+    createNote(title, body) {
+        const note = new Note({...});
+        this.setState({notes: this.state.push(note)});
+    }
+
     render() {
         return <this.state.view
             notes={this.state.notes}
             showNoteView={this.showNoteView}
             showListView={this.showListView}
+            createNote={this.createNote}
         />;
     }
 }
